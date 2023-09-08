05154fc6d5b8e5f7911b11f6f9d589c17cb62039
Kamal Sharma <kamalsharma25091994@gmail.com>
2023-09-08 18:20:08 +0530

    working

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-    <select>
-      <option>10</option>
-      <option>20</option>
-      <option>30</option>
-      <option>50</option>
-      <option>100</option>
-    </select>
+         <AllRoutes/>
     </div>
   );
 }
